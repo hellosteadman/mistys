b97aaee0e311782b6b562704a97ac12f5129e04e
Mark Steadman <marksteadman@me.com>
2014-03-11 20:25:46 +0000

    Only make opacity changes for WebKit browsers

--- js/functions.js
+++ js/functions.js
@@ -70,7 +70,7 @@ jQuery(document).ready(
 					var cTop = parseInt($('body').scrollTop()) + nHeight;
 					var cBottom = $('body').outerHeight() - cTop;
 					
-					if ($.browser.msie) {
+					if (!$.browser.webkit) {
 						return;
 					}
 					
